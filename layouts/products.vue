3ca6fa7cc530a4c74204e506495fed92b04dc315
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-25 23:37:31 +0800

    feat: enhanced layout for products

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">Nuxt JQ MERCH </NuxtLink>
 
+        <!-- // this behavior will not be like a single page application or SPA -->
+        <!-- <a href="/about">normal link -- about page</a> -->
+      </nav>
+    </header>
+    <div>
+      <slot />
     </div>
+    <footer>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </footer>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
