76b132ed7a0b2b465573a00f0e3e6967c95626aa
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-28 21:37:57 +0800

    feat: display product details

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <p>product details for {{ id }}</p>
+        <!-- <p>product details for {{ id }}</p> -->
+         <p>{{ product.title }}</p>
+         <p>{{ product.price }}</p>
+         <p>{{ product.id }}</p>
     </div>
 </template>
 
@@ -11,7 +14,7 @@
 
     // fetch  the product using id
     const {data: product} = await useFetch(url)
-    
+
     definePageMeta({
         layout: 'products'
     });
