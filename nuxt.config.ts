3595469b4dbb47633924c63d3f2066b38646ab3f
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-28 22:26:24 +0800

    feat: added material icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules:['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Nuxt JQ DEV",
+      meta: [{ name: "description", content: "Everything about the merch" }],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+});
