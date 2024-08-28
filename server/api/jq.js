7e4a58f3c950439b45db4ab5fb5e3f0f06ba8ba4
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-28 22:48:42 +0800

    get the body in the server part and use body

--- server/api/jq.js
+++ server/api/jq.js
@@ -1,9 +1,12 @@
- export default defineEventHandler((event) => {
+ export default defineEventHandler(async (event) => {
 
     // handle query params
     // const { name } = useQuery(event) old method
     const { name } = getQuery(event); //new method
+
+
+    const {age, name:name_post} = await readBody(event)
     return {
-      message: `Hello ${name}`,
+      message: `Hello ${name_post}, you are ${age} years old`,
     };
  })
