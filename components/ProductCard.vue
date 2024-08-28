760b5e14551e8081536b0822104f06c6f1c7efc2
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-28 21:43:06 +0800

    feat: created component for product card

---
+++ components/ProductCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+    const { product} = defineProps(['product'])
+</script>
+
+<style scoped>
+
+</style>
