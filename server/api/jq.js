d0c0c1cf806800dab69ed706302c580a1864146f
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-28 23:01:50 +0800

    use fetch to get currencies api

--- server/api/jq.js
+++ server/api/jq.js
@@ -4,7 +4,9 @@
     // const { name } = useQuery(event) old method
     const { name } = getQuery(event); //new method
 
-
+    const { data } = await $fetch(
+      "https://api.currencyapi.com/v3/currencies?apikey=cur_live_27e9nuVF3j90eubaFn02niLvPIB9m2FCTTp9qbrF"
+    );
     const {age, name:name_post} = await readBody(event)
     return {
       message: `Hello ${name_post}, you are ${age} years old`,
