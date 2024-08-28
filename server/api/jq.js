dd001e8c4a73532d8c5ed6adccb66400825d14cb
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-28 23:02:49 +0800

    feat: fetch data from the api currencies

--- server/api/jq.js
+++ server/api/jq.js
@@ -7,8 +7,11 @@
     const { data } = await $fetch(
       "https://api.currencyapi.com/v3/currencies?apikey=cur_live_27e9nuVF3j90eubaFn02niLvPIB9m2FCTTp9qbrF"
     );
+
+
+
     const {age, name:name_post} = await readBody(event)
     return {
-      message: `Hello ${name_post}, you are ${age} years old`,
+      message: {message: `Hello ${name_post}, you are ${age} years old`, data},
     };
  })
