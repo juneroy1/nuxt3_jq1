5e6612eda72cce08294d1b5fd49161c020eda7d8
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-30 14:33:02 +0800

    feat: added new link for projects page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,11 @@
     <div>
         <header class="shadow-sm bg-white">
         <nav class="container mx-auto p-4 flex justify-between">
-          <NuxtLink to="/" class="font-bold">Nuxt JQ dev </NuxtLink>
+          <NuxtLink to="/" class="font-bold">JQ Dev</NuxtLink>
             <ul class="flex gap-4">
                 <li><NuxtLink to="/">Home</NuxtLink></li>
                 <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/projects">Projects</NuxtLink></li>
                 <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
             </ul>
         
